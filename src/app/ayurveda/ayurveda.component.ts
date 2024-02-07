c57fed994a85a563e39c69442caffb76e8f27d03
Jeannine Christin Walz <jeanninechristinwalz@Jeannines-MacBook-Air.local>
2024-02-07 15:36:23 +0100

    feat: dosha text version 1

--- src/app/ayurveda/ayurveda.component.ts
+++ src/app/ayurveda/ayurveda.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ayurveda',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ayurveda.component.html',
   styleUrl: './ayurveda.component.scss'
 })
 export class AyurvedaComponent {
+  doshas = [
+    { title: 'VATA', elemente: 'Luft & Äther', prinzip: 'Das Prinzip der Bewegung' },
+    { title: 'PITTA', elemente: 'Feuer & Wasser', prinzip: 'Das Prinzip der Umwandlung' },
+    { title: 'KAPHA', elemente: 'Wasser & Erde', prinzip: 'Das Prinzip der Stabiltät' }
+  ];
 
 }
